216d5d83d2131bf231924e3ad5cd26b83681f605
hezron <hezzykialo.official.ke@gmail.com>
2022-06-20 14:40:50 +0300

    template for search and display business at front-end

--- hapen/views.py
+++ hapen/views.py
@@ -3,10 +3,19 @@ from .models import Business
 
 # Create your views here.
 def home(request):
-    return render(request, 'home/home.html')
+    context = {
+        "business": Business.objects.all(),
+    }
+    return render(request, 'home/home.html',context)
 
 def business_search(request):
     if 'search_business' in request.GET and request.GET['search_business']:
         search_term = request.GET.get('search_business')
         searched_business = Business.search_by_title(search_term)
+        message = f"{search_term}"
+
+        return render(request, 'home/business_search.html',{"message":message,"business": searched_business})
+    else:
+        message = "Your haven't search for any business"
+
     return render(request, 'home/business_search.html')
