d6e562b68adf9fb8180abca46fa05c248887d634
isaac mariga <isaacwangombe5@gmail.com>
2022-06-20 12:22:00 +0300

    Model edits

--- happenings/settings.py
+++ happenings/settings.py
@@ -40,9 +40,10 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     # third parties libraries
-    'phonenumber_field',
+    # 'phonenumber_field',
     'crispy_forms',
     'location_field.apps.DefaultConfig',
+
 
     # own
     'hapen',
@@ -85,8 +86,10 @@ WSGI_APPLICATION = 'happenings.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'neighbourhood',
+        'USER': 'mariga',
+    'PASSWORD':'password',
     }
 }
 
@@ -115,7 +118,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Africa/Nairobi'
 
 USE_I18N = True
 
@@ -140,3 +143,4 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 LOGIN_REDIRECT_URL = 'home'
 
 LOGIN_URL = 'login'
+
